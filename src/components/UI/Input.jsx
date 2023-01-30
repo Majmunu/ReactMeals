e8f2b0057fe9e0cf68770149bd7a9ab268d8fa7d
Oluweaseun <wangc2692@gmail.com>
2023-01-30 18:31:56 +0800

    完成表单组件

---
+++ src/components/UI/Input.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './Input.module.scss'
+export default function Input(props) {
+  return (
+    <div className={styles.input}>
+        <label htmlFor={props.input.id}>{props.label}</label>
+        <input id={props.input.id} {...props.input} />
+    </div>
+  )
+}
